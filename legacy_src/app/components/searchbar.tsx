184ed86305b3a537dab416f51b9e02e843d9f45c
limchaehwan <chlim20@naver.com>
2025-09-06 04:35:16 +0900

    UI 구현(src 파일 교체)

--- legacy_src/app/components/searchbar.tsx
+++ legacy_src/app/components/searchbar.tsx
@@ -1,18 +1,24 @@
 //상호 작용이 일어나는 클라이언트 컴퍼넌트임(클라이언트 컴퍼넌트 명시)
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Searchbar() {
+  const router = useRouter();
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
